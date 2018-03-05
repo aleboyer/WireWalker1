ed99d21d3c375e83d3be6e9f15a8fcfc3a4bbd45
Maddie <mhamann@ucsd.edu>
2018-03-05 11:41:45 -0800

    Fixed a bug in mergefields_WW. Added capabilities to get_WW_data; now it adds columns of NaN wherever there is a gap larger than 1 hour and removes any columns with duplicate timestamps

--- Toolbox/get_WW_data.m
+++ Toolbox/get_WW_data.m
@@ -1,7 +1,12 @@
 function WWgrid_out = get_WW_data(WWmeta,begtime,endtime)
+if endtime<begtime
+    warning('Dates are out of order')
+    WWgrid_out = [];
+    return
+end
 % Function to grab gridded WW data.
 % WWmeta should have fields generated in process_PROJECTNAME.m
-% 
+%
 % begtime and endtime are the range of days to plot (matlab datenum format)
 %
 % Created by M. Hamann 9/12/17
@@ -17,6 +22,27 @@ for ii = id_grab;
     id1 = [];
     WWgrid_out = mergefields_WW(WWgrid_out,WWgrid,id1,id2);
     clear WWgrid
+end
+
+id = find(diff(WWgrid_out.time) ~= 0);
+vars = fieldnames(WWgrid_out);
+if length(id)<length(WWgrid_out.time)
+    for ii = 1:length(vars);
+        [m n] = size(WWgrid_out.(vars{ii}));
+        if n~=1;
+            WWgrid_out.(vars{ii}) = WWgrid_out.(vars{ii})(:,id);
+        end
+    end
+end
+
+id = find(diff(WWgrid_out.time)>(1/24)); 
+if~isempty(id);
+    for ii = 1:length(vars);
+        [m n] = size(WWgrid_out.(vars{ii}));
+        if n~=1;
+            WWgrid_out.(vars{ii}) = add_nancolumns(WWgrid_out.(vars{ii}),id);
+        end
+    end
 end
 
 
--- Toolbox/mergefields_WW.m
+++ Toolbox/mergefields_WW.m
@@ -2,6 +2,7 @@ function WWgrid_out = mergefields_WW(WWgrid1,WWgrid2,id1,id2);
 %   function dat = mergefields_WW(WWgrid1,WWgrid2,id);
 %   Merge structures with data in the standard WWgrid format
 % M Hamann 9/14/17
+% updated 3/4/18
 
 if nargin<4
     id2 = [];
@@ -10,51 +11,43 @@ if nargin<3
     id1 = [];
 end
 
+vars = fieldnames(WWgrid2);
+for ii = 1:length(vars)
+    [m,n] = size(WWgrid2.(vars{ii}));
+    if n>1
+        WWgrid2.(vars{ii}) = WWgrid2.(vars{ii})(:,id2);
+    end
+end
+
 if isempty(WWgrid1)
     WWgrid_out=WWgrid2;
-    names = fieldnames(WWgrid2);
-    for ii=1:length(names);
-        data2 = getfield(WWgrid2,names{ii});
-        [M2,N2]=size(data2);
-        if ~isempty(id2)
-            N2 = length(id2);
-            id2tmp = id2;
-        else
-            id2tmp = 1:N2;
-        end
-        if strcmp(names{ii},'z')
-            WWgrid_out.z = WWgrid2.z;
-        else
-            WWgrid_out = setfield(WWgrid_out,names{ii},data2(:,id2tmp));
+    return
+end
+
+
+vars = fieldnames(WWgrid1);
+if ~isempty(id1)
+    for ii = 1:length(vars)
+        [m,n] = size(WWgrid1.(vars{ii}));
+        if n>1
+            WWgrid1.(vars{ii}) = WWgrid1.(vars{ii})(:,id1);
         end
     end
-    
-    return;
-end;
+end
 
-names = fieldnames(WWgrid1);
 WWgrid_out=WWgrid1;
 
-for ii=1:length(names);
-    data1 = getfield(WWgrid1,names{ii});
-    data2 = getfield(WWgrid2,names{ii});
+for ii=1:length(vars);
+    data1 = getfield(WWgrid1,vars{ii});
+    data2 = getfield(WWgrid2,vars{ii});
+    
     [M1,N1]=size(data1);
-    if ~isempty(id1)
-        N1 = length(id1);
-        id1tmp = id1;
-    else
-        id1tmp = 1:N1;
-    end
+    id1tmp = 1:N1;
     
     [M2,N2]=size(data2);
-    if ~isempty(id2)
-        N2 = length(id2);
-        id2tmp = id2;
-    else
-        id2tmp = 1:N2;
-    end
+    id2tmp = 1:N2;
     
-    if strcmp(names{ii},'z')
+    if strcmp(vars{ii},'z')
         if M1>M2, WWgrid_out.z = WWgrid1.z; else WWgrid_out.z = WWgrid2.z; end
     else
         if M1>M2
@@ -64,7 +57,7 @@ for ii=1:length(names);
         else
             data1 =data1(:,id1tmp); data2 =data2(:,id2tmp);
         end;
-        WWgrid_out = setfield(WWgrid_out,names{ii},[data1 data2]);
+        WWgrid_out = setfield(WWgrid_out,vars{ii},[data1 data2]);
     end
     
     
